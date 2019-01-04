b7ca9735e345102d5291e47ff7a4905a3c51b948
Nena Raab <nena.raab@sap.com>
2019-01-04 23:24:52 +0100

    initial version of spring-xsuaa-test lib

---
+++ spring-xsuaa-test/src/test/java/com/sap/cloud/security/xsuaa/test/TestConstants.java
@@ -0,0 +1,11 @@
+package com.sap.cc.jwtgenerator;
+
+public interface TestConstants {
+    String DUMMY_SCOPE = "some scope";
+    String ANOTHER_SCOPE = "some other scope";
+    String DUMMY_ATTRIBUTE = "confidentiality_level";
+    String DUMMY_ATTRIBUTE_VALUE = "STRICTLY_CONFIDENTIAL";
+    String ANOTHER_ATTRIBUTE = "costcenter";
+    String ANOTHER_ATTRIBUTE_VALUE = "100";
+    String ANOTHER_ATTRIBUTE_VALUE_2 = "200";
+}
