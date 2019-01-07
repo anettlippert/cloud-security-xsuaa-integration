SAP/cloud-security-services-integration-library

Ease enhancement of TokenAuthenticationConverter

spring-xsuaa/src/main/java/com/sap/cloud/security/xsuaa/token/TokenAuthenticationConverter.java
@@ -1,42 +1,58 @@
+/**
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved.
+ * This file is licensed under the Apache Software License,
+ * v. 2 except as noted otherwise in the LICENSE file
+ * https://github.com/SAP/cloud-security-xsuaa-integration/blob/master/LICENSE
+ */
 package com.sap.cloud.security.xsuaa.token;
 
-import com.sap.cloud.security.xsuaa.XsuaaServiceConfiguration;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import com.sap.cloud.security.xsuaa.XsuaaServiceConfiguration;
 
+/**
+ * Converter for xsuaa jwt token that stores authorization data like scopes inside the token.
+ */
 public class TokenAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
-    private String appId;
+	protected String appId;
 
-    public TokenAuthenticationConverter(String appId) {
-        this.appId = appId;
-    }
+	public TokenAuthenticationConverter(String appId) {
+		this.appId = appId;
+	}
 
-    public TokenAuthenticationConverter(XsuaaServiceConfiguration xsuaaServiceConfiguration) {
-        this.appId = xsuaaServiceConfiguration.getAppId();
-    }
+	public TokenAuthenticationConverter(XsuaaServiceConfiguration xsuaaServiceConfiguration) {
+		this.appId = xsuaaServiceConfiguration.getAppId();
+	}
 
-
-    public final AbstractAuthenticationToken convert(Jwt jwt) {
-        Collection<GrantedAuthority> authorities = extractAuthorities(jwt);
-        return new AuthenticationToken(appId, jwt, authorities);
-    }
+	public final AbstractAuthenticationToken convert(Jwt jwt) {
+		return new AuthenticationToken(appId, jwt, extractAuthorities(jwt));
+	}
 
     protected Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
-        Collection<String> scopes = this.getScopes(jwt);
-        return scopes.stream().map(authority -> authority).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+        Collection<String> scopeAuthorities = getScopes(jwt);
+        Collection<String> otherAuthorities = extractAuthorities(new TokenImpl(jwt, appId));
+
+        Stream<String> authorities = Stream.of(scopeAuthorities, otherAuthorities).flatMap(Collection::stream);
+        return authorities.map(authority -> authority).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
     }
 
-    private Collection<String> getScopes(Jwt jwt) {
-        List<String> scopesList = jwt.getClaimAsStringList(Token.CLAIM_SCOPES);
-        return scopesList != null ? scopesList : Collections.emptyList();
-    }
+	protected Collection<String> extractAuthorities(Token token) {
+        return Collections.emptyList();
+	}
+
+	protected Collection<String> getScopes(Jwt jwt) {
+		List<String> scopesList = jwt.getClaimAsStringList(Token.CLAIM_SCOPES);
+		return scopesList != null ? scopesList : Collections.emptyList();
+	}
 }

spring-xsuaa/src/test/java/com/sap/cloud/security/xsuaa/token/TokenAuthenticationConverterTest.java
@@ -1,0 +1,99 @@
+/**
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved.
+ * This file is licensed under the Apache Software License,
+ * v. 2 except as noted otherwise in the LICENSE file
+ * https://github.com/SAP/cloud-security-xsuaa-integration/blob/master/LICENSE
+ */
+package com.sap.cloud.security.xsuaa.token;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import com.nimbusds.jwt.JWTClaimsSet;
+
+public class TokenAuthenticationConverterTest {
+	private JWTClaimsSet.Builder claimsSetBuilder = null;
+	private String xsAppName = "my-app-name!400";
+	private TokenAuthenticationConverter tokenConverter;
+	String scopeAdmin = xsAppName + "." + "Admin";
+	String scopeRead = xsAppName + "." + "Read";
+    private Jwt jwtSamlWithXsUserAttributes;
+
+	@Before
+	public void setup() throws Exception {
+		claimsSetBuilder = new JWTClaimsSet.Builder().issueTime(new Date()).expirationTime(JwtGenerator.NO_EXPIRE);
+		tokenConverter = new TokenAuthenticationConverter(xsAppName);
+        jwtSamlWithXsUserAttributes = JwtGenerator.createFromTemplate("/saml.txt");
+	}
+
+	@Test
+	public void extractAuthoritiesWithoutScopes() throws Exception {
+		Jwt jwt = JwtGenerator.createFromClaims(claimsSetBuilder.build());
+
+		AbstractAuthenticationToken authenticationToken = tokenConverter.convert(jwt);
+		assertThat(authenticationToken.getAuthorities().size(), is(0));
+	}
+
+	@Test
+	public void extractAuthoritiesWithScopes() throws Exception {
+		List<String> scopesList = new ArrayList<>();
+		scopesList.add(scopeAdmin);
+		scopesList.add(scopeRead);
+		claimsSetBuilder.claim(Token.CLAIM_SCOPES, scopesList);
+		Jwt jwt = JwtGenerator.createFromClaims(claimsSetBuilder.build());
+
+		AbstractAuthenticationToken authenticationToken = tokenConverter.convert(jwt);
+		assertThat(authenticationToken.getAuthorities().size(), is(2));
+		assertThat(authenticationToken.getAuthorities(), hasItem(new SimpleGrantedAuthority(scopeRead)));
+		assertThat(authenticationToken.getAuthorities(), hasItem(new SimpleGrantedAuthority(scopeAdmin)));
+	}
+
+	@Test
+	public void extractCustomAuthoritiesWithScopes() throws Exception {
+        tokenConverter = new MyTokenAuthenticationConverter(xsAppName, "cost-center", "country");
+
+		AbstractAuthenticationToken authenticationToken = tokenConverter.convert(jwtSamlWithXsUserAttributes);
+		assertThat(authenticationToken.getAuthorities().size(), is(7));
+		assertThat(authenticationToken.getAuthorities(), hasItem(new SimpleGrantedAuthority("ATTR:COST-CENTER=0815")));
+		assertThat(authenticationToken.getAuthorities(), hasItem(new SimpleGrantedAuthority("ATTR:COST-CENTER=4711")));
+		assertThat(authenticationToken.getAuthorities(), hasItem(new SimpleGrantedAuthority("ATTR:COUNTRY=Germany")));
+        assertThat(authenticationToken.getAuthorities(), hasItem(new SimpleGrantedAuthority("java-hello-world.Delete")));
+	}
+
+	private static class MyTokenAuthenticationConverter extends TokenAuthenticationConverter {
+
+		protected String[] xsUserAttributes;
+
+		public MyTokenAuthenticationConverter(String appId, String... xsUserAttributes) {
+			super(appId);
+			this.xsUserAttributes = xsUserAttributes;
+		}
+
+		@Override
+		protected Collection<String> extractAuthorities(Token token) {
+			Set<String> newAuthorities = new HashSet<>();
+			for (String attribute : xsUserAttributes) {
+				String[] xsUserAttributeValues = token.getXSUserAttribute(attribute);
+				if (xsUserAttributeValues != null) {
+					for (String xsUserAttributeValue : xsUserAttributeValues) {
+						newAuthorities.add(getSidForAttributeValue(attribute, xsUserAttributeValue));
+					}
+				}
+			}
+			return newAuthorities;
+		}
+
+		public static String getSidForAttributeValue(String attributeName, String attributeValue) {
+			return "ATTR:" + attributeName.toUpperCase() + "=" + attributeValue;
+		}
+	}
+}

anettlippert/cloud-security-xsuaa-integration

spring-xsuaa/src/main/java/com/sap/cloud/security/xsuaa/token/TokenAuthenticationConverter.java
@@ -1,42 +1,58 @@
+/**
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved.
+ * This file is licensed under the Apache Software License,
+ * v. 2 except as noted otherwise in the LICENSE file
+ * https://github.com/SAP/cloud-security-xsuaa-integration/blob/master/LICENSE
+ */
 package com.sap.cloud.security.xsuaa.token;
 
-import com.sap.cloud.security.xsuaa.XsuaaServiceConfiguration;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+import com.sap.cloud.security.xsuaa.XsuaaServiceConfiguration;
 
+/**
+ * Converter for xsuaa jwt token that stores authorization data like scopes inside the token.
+ */
 public class TokenAuthenticationConverter implements Converter<Jwt, AbstractAuthenticationToken> {
 
-    private String appId;
+	protected String appId;
 
-    public TokenAuthenticationConverter(String appId) {
-        this.appId = appId;
-    }
+	public TokenAuthenticationConverter(String appId) {
+		this.appId = appId;
+	}
 
-    public TokenAuthenticationConverter(XsuaaServiceConfiguration xsuaaServiceConfiguration) {
-        this.appId = xsuaaServiceConfiguration.getAppId();
-    }
+	public TokenAuthenticationConverter(XsuaaServiceConfiguration xsuaaServiceConfiguration) {
+		this.appId = xsuaaServiceConfiguration.getAppId();
+	}
 
+	public final AbstractAuthenticationToken convert(Jwt jwt) {
+		return new AuthenticationToken(appId, jwt, extractAuthorities(jwt));
+	}
 
-    public final AbstractAuthenticationToken convert(Jwt jwt) {
-        Collection<GrantedAuthority> authorities = extractAuthorities(jwt);
-        return new AuthenticationToken(appId, jwt, authorities);
-    }
+	protected Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
+		Collection<String> scopeAuthorities = getScopes(jwt);
+		Collection<String> customAuthorities = getCustomAuthorities(new TokenImpl(jwt, appId));
 
-    protected Collection<GrantedAuthority> extractAuthorities(Jwt jwt) {
-        Collection<String> scopes = this.getScopes(jwt);
-        return scopes.stream().map(authority -> authority).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
-    }
+		Stream<String> authorities = Stream.of(scopeAuthorities, customAuthorities).flatMap(Collection::stream);
+		return authorities.map(authority -> authority).map(SimpleGrantedAuthority::new).collect(Collectors.toList());
+	}
 
-    private Collection<String> getScopes(Jwt jwt) {
-        List<String> scopesList = jwt.getClaimAsStringList(Token.CLAIM_SCOPES);
-        return scopesList != null ? scopesList : Collections.emptyList();
-    }
+	protected Collection<String> getCustomAuthorities(Token token) {
+		return Collections.emptyList();
+	}
+
+	protected Collection<String> getScopes(Jwt jwt) {
+		List<String> scopesList = jwt.getClaimAsStringList(Token.CLAIM_SCOPES);
+		return scopesList != null ? scopesList : Collections.emptyList();
+	}
 }

spring-xsuaa/src/test/java/com/sap/cloud/security/xsuaa/token/TokenAuthenticationConverterTest.java
@@ -1,0 +1,99 @@
+/**
+ * Copyright (c) 2018 SAP SE or an SAP affiliate company. All rights reserved.
+ * This file is licensed under the Apache Software License,
+ * v. 2 except as noted otherwise in the LICENSE file
+ * https://github.com/SAP/cloud-security-xsuaa-integration/blob/master/LICENSE
+ */
+package com.sap.cloud.security.xsuaa.token;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+import com.nimbusds.jwt.JWTClaimsSet;
+
+public class TokenAuthenticationConverterTest {
+	private JWTClaimsSet.Builder claimsSetBuilder = null;
+	private String xsAppName = "my-app-name!400";
+	private TokenAuthenticationConverter tokenConverter;
+	String scopeAdmin = xsAppName + "." + "Admin";
+	String scopeRead = xsAppName + "." + "Read";
+	private Jwt jwtSamlWithXsUserAttributes;
+
+	@Before
+	public void setup() throws Exception {
+		claimsSetBuilder = new JWTClaimsSet.Builder().issueTime(new Date()).expirationTime(JwtGenerator.NO_EXPIRE);
+		tokenConverter = new TokenAuthenticationConverter(xsAppName);
+		jwtSamlWithXsUserAttributes = JwtGenerator.createFromTemplate("/saml.txt");
+	}
+
+	@Test
+	public void extractAuthoritiesWithoutScopes() throws Exception {
+		Jwt jwt = JwtGenerator.createFromClaims(claimsSetBuilder.build());
+
+		AbstractAuthenticationToken authenticationToken = tokenConverter.convert(jwt);
+		assertThat(authenticationToken.getAuthorities().size(), is(0));
+	}
+
+	@Test
+	public void extractAuthoritiesWithScopes() throws Exception {
+		List<String> scopesList = new ArrayList<>();
+		scopesList.add(scopeAdmin);
+		scopesList.add(scopeRead);
+		claimsSetBuilder.claim(Token.CLAIM_SCOPES, scopesList);
+		Jwt jwt = JwtGenerator.createFromClaims(claimsSetBuilder.build());
+
+		AbstractAuthenticationToken authenticationToken = tokenConverter.convert(jwt);
+		assertThat(authenticationToken.getAuthorities().size(), is(2));
+		assertThat(authenticationToken.getAuthorities(), hasItem(new SimpleGrantedAuthority(scopeRead)));
+		assertThat(authenticationToken.getAuthorities(), hasItem(new SimpleGrantedAuthority(scopeAdmin)));
+	}
+
+	@Test
+	public void extractCustomAuthoritiesWithScopes() throws Exception {
+		tokenConverter = new MyTokenAuthenticationConverter(xsAppName, "cost-center", "country");
+
+		AbstractAuthenticationToken authenticationToken = tokenConverter.convert(jwtSamlWithXsUserAttributes);
+		assertThat(authenticationToken.getAuthorities().size(), is(7));
+		assertThat(authenticationToken.getAuthorities(), hasItem(new SimpleGrantedAuthority("ATTR:COST-CENTER=0815")));
+		assertThat(authenticationToken.getAuthorities(), hasItem(new SimpleGrantedAuthority("ATTR:COST-CENTER=4711")));
+		assertThat(authenticationToken.getAuthorities(), hasItem(new SimpleGrantedAuthority("ATTR:COUNTRY=Germany")));
+		assertThat(authenticationToken.getAuthorities(), hasItem(new SimpleGrantedAuthority("java-hello-world.Delete")));
+	}
+
+	private static class MyTokenAuthenticationConverter extends TokenAuthenticationConverter {
+
+		protected String[] xsUserAttributes;
+
+		public MyTokenAuthenticationConverter(String appId, String... xsUserAttributes) {
+			super(appId);
+			this.xsUserAttributes = xsUserAttributes;
+		}
+
+		@Override
+		protected Collection<String> getCustomAuthorities(Token token) {
+			Set<String> newAuthorities = new HashSet<>();
+			for (String attribute : xsUserAttributes) {
+				String[] xsUserAttributeValues = token.getXSUserAttribute(attribute);
+				if (xsUserAttributeValues != null) {
+					for (String xsUserAttributeValue : xsUserAttributeValues) {
+						newAuthorities.add(getSidForAttributeValue(attribute, xsUserAttributeValue));
+					}
+				}
+			}
+			return newAuthorities;
+		}
+
+		public static String getSidForAttributeValue(String attributeName, String attributeValue) {
+			return "ATTR:" + attributeName.toUpperCase() + "=" + attributeValue;
+		}
+	}
+}